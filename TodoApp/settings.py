0e913525a212b78324f8c5f3236a6f564dcd68ac
Sujit Kumar <39940491+sujeet2395@users.noreply.github.com>
2021-04-04 15:04:02 +0530

    Update settings.py
    changes done

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -75,32 +75,14 @@ WSGI_APPLICATION = 'TodoApp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-'''
-'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'letsdaredb',
-        'USER': 'postgres',
-        'PASSWORD':'Sharma@2395',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
-'''
+
 '''
 'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 '''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dolckf42f5rc9',
-        'USER': 'qdsfmrgzwpzbsr',
-        'PASSWORD':'d31af3e676fdfbde956733359d977832e235d1958700496353bc04d46c6e5d90',
-        'HOST':'ec2-18-206-20-102.compute-1.amazonaws.com',
-        'PORT':'5432',
-    }
-}
+
 
 
 # Password validation
